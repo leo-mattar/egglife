4e26fa12124a2b66d8994e6ea1b6778946448ea8
leo-mattar <126821704+leo-mattar@users.noreply.github.com>
2025-04-04 07:49:53 +0100

    Add files via upload

---
+++ assets/js/culinary-modal-2.0.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function showModal() {
+    const modal = document.querySelector(".c-nw-modal");
+    if (!modal) return;
+
+    if (sessionStorage.getItem("nwModalShown")) return;
+    if (window.innerWidth > 767) return;
+
+    setTimeout(() => {
+      modal.classList.add("is-open");
+      window.Scroll.stop();
+    }, 4000);
+
+    const closeButton = document.querySelector(".c-icon.nw-modal-close");
+    if (closeButton) {
+      closeButton.addEventListener("click", () => {
+        modal.classList.remove("is-open");
+        sessionStorage.setItem("nwModalShown", "true");
+        window.Scroll.start();
+      });
+    }
+  }
+
+  showModal();
+});
